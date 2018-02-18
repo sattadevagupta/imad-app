64deac3e847cedde43d627b252f04fad7fb046f3
sattadevagupta <36528463+sattadevagupta@users.noreply.github.com>
2018-02-18 16:02:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one',function(req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
 });
 
 app.get('/article-two',function(req,res){
