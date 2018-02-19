2e441a86c8f036bdecb712377ce0add85d2fee4d
sattadevagupta <36528463+sattadevagupta@users.noreply.github.com>
2018-02-19 18:33:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,10 @@
 //Counter code
-var button =documet.getElementById('counter');
+var button =document.getElementById('counter');
 var counter =0;
 
 button.onclick=function(){
     counter=counter+1;
-    var span =documet.getElementById('count');
+    var span =document.getElementById('count');
 
     span.innerHTML = counter.toString();
     
