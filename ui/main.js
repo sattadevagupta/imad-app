210fca832aca40d5e37d90a8badb292569227c67
sattadevagupta <36528463+sattadevagupta@users.noreply.github.com>
2018-02-19 18:32:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
-console.log('Loaded!');
+//Counter code
+var button =documet.getElementById('counter');
+var counter =0;
+
+button.onclick=function(){
+    counter=counter+1;
+    var span =documet.getElementById('count');
+
+    span.innerHTML = counter.toString();
+    
+};
